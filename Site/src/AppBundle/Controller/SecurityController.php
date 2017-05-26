ae4f30899206d8e644b22cfe1f5eec3cf9b72ae8
Andrei Tenescu <teny96@gmail.com>
2017-05-27 00:01:23 +0300

    Began work on login system

---
+++ Site/src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: teni9
+ * Date: 5/26/2017
+ * Time: 11:42 PM
+ */
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    /**
+    * @Route("/login", name="login")
+    */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
