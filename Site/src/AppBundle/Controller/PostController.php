2a8fed5f9df20cbd07616c496ee9a8c7c53489bb
Andrei Tenescu <teny96@gmail.com>
2017-05-27 16:54:58 +0300

    Please don't die

--- Site/src/AppBundle/Controller/PostController.php
+++ Site/src/AppBundle/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Post;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -59,9 +60,37 @@ class PostController extends Controller
     /**
      * @Route("/make_post")
      */
-    public function makeAction()
+    public function makeAction(Request $request)
     {
-        return $this->render('Post/make.html.twig');
+        // 1) build the form
+        $post = new Post();
+
+        $form = $this->createFormBuilder($post)
+            ->add('nume', TextType::class)
+            ->add('categorie', TextType::class)
+            ->add('descriere', TextType::class)
+            ->getForm();
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render(
+            'Post/make.html.twig',
+            array('form' => $form->createView())
+        );
+
+        // return $this->render('posts/make.html.twig');
     }
 
     /**
